eedf2fb7e7c2b38fe9bc8b227b173bcf4d50772a
Harin23 <63739311+Harin23@users.noreply.github.com>
2026-09-19 15:03:38 -0600

    continue on space char and break on non digit as required

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main() {
     int ch;
 
     while ((ch = getchar()) != EOF) {
+        if (ch == ' ') continue;
+        if (!isdigit(ch)) break;
         putchar(ch);
     }
+
     return 0;
 }
